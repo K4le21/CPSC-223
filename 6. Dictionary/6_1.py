db6e074a678a577a697511582ba12b344c8015b9
Kyle Chan <116043633+K4le21@users.noreply.github.com>
2022-10-20 13:40:07 -0700

    Create 6_1.py

---
+++ 6. Dictionary/6_1.py
@@ -0,0 +1,7 @@
+friend_info = {"First name" : "James",
+                "Last name" : "Lee",
+                "Age" : 20,
+                "City" : "Fullerton"}
+
+for key, value in friend_info.items():
+    print (key + ": " + str(value))
