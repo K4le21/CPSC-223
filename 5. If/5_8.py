a71883983bba0cefead02259e00bab1a4ab12ad2
Kyle Chan <116043633+K4le21@users.noreply.github.com>
2022-10-20 13:23:28 -0700

    Create 5_8.py

---
+++ 5. If/5_8.py
@@ -0,0 +1,6 @@
+userList = ["Will", "Jaden", "admin", "Ryan", "Alex"]
+for x in userList:
+    if x == "admin":
+        print ("Hello " + x + ", would you like to see a status report?")
+    else:
+        print ("Welcome " + x + ", thank you for logging in again.")
